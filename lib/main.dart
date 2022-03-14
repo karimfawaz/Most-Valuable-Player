ecfaa701ba5bd5979e8d932c4b3ed1c96902cd36
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2022-03-14 01:44:44 +0000

    I finally found a way to make it work

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:html/parser.dart' as parser;
-import 'package:http/http.dart' as http;
 
-void main() => runApp(MaterialApp(
-    theme: ThemeData(
-      scaffoldBackgroundColor: Colors.green[100],
-      primaryColor: Colors.green,
-      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.green),
-    ),
-    home: const MyApp()));
+import 'package:mvp/providers/PlayerManager.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MVP());
+
+class MVP extends StatelessWidget {
+  const MVP({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        // ChangeNotifierProvider<Orders>(create: (_) => Orders()),
+        // class Orders with ChangeNotifier {
+
+        ChangeNotifierProvider<PlayerManager>(create: (_) => PlayerManager()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.green[100],
+          primaryColor: Colors.green,
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: Colors.green),
+        ),
+        home: const MyApp(),
+        // routes: {
+        //   // ContactUsScreen.routeName: (ctx) => ContactUsScreen(),
+        //   // onTap: () {
+        //   //               Navigator.of(context)
+        //   //                   .pushNamed(AppointmentScreen.routeName);
+        // }
+      ),
+    );
+  }
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -19,88 +45,19 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // Strings to store the extracted Article titles
-  String result1 = 'Result 1';
-  String result2 = 'Result 2';
+  String result1 =
+      'https://www.axelspringer.com/data/uploads/2019/04/transfermarkt.jpg';
+  String result2 =
+      'https://www.axelspringer.com/data/uploads/2019/04/transfermarkt.jpg';
   String result3 = 'Result 3';
 
   // boolean to show CircularProgressIndication
   // while Web Scraping awaits
   bool isLoading = false;
 
-  Future<List<String>> extractData() async {
-    // Getting the response from the targeted url
-    final response = await http.Client().get(Uri.parse(
-        'https://www.transfermarkt.co.uk/spieler-statistik/wertvollstespieler/marktwertetop?page=' +
-            '1'));
-
-    // Status Code 200 means response has been received successfully
-    if (response.statusCode == 200) {
-      // Getting the html document from the response
-      var document = parser.parse(response.body);
-      try {
-        // Scraping the first article title
-        var table = document
-            .getElementsByClassName('items')[0]
-            .children[1]; // thead or tbody (we want tbody so 1)
-        var player = table.children[0]; //player index (row selector)
-        var playerDetails = player
-            .children[1]
-            // index number is 0 we want 1 for player details
-            .children[0] // td container
-            .children[0]; // inline table
-        var playerName = playerDetails
-            .children[0] // tbody child
-            .children[1] // first child is the image second is the name
-            .children[0] // inside the td there is a link for the player's name
-            .innerHtml;
-        var playerPosition = playerDetails.children[1].children[0].innerHtml;
-        var playerValue = player.children[5].children[0].innerHtml;
-        var playerValueNumb =
-            playerValue.replaceAll("£", "").replaceAll("m", "");
-        var playerLink = playerDetails
-            .children[0] // tbody child
-            .children[1] // first child is the image second is the name
-            .innerHtml
-            .split(
-                '"')[3]; // inside the td there is a link for the player's name
-        // print(playerValue);
-        // print(playerValueNumb);
-        var newLink = await http.Client()
-            .get(Uri.parse('https://www.transfermarkt.co.uk' + playerLink));
-
-        var newDocument = parser.parse(newLink.body);
-
-        var playerTeamImage = newDocument
-            .getElementsByClassName("dataZusatzImage")[0]
-            .children[0]
-            .innerHtml
-            .split('"')[1]
-            .split(',')[0]
-            .trim()
-            .split(' ')[0];
-        var playerImage =
-            newDocument.getElementsByClassName("modal-trigger")[0].innerHtml;
-
-        print(playerImage);
-        // print(playerTeamImage);
-
-        // print(playerName);
-        // print(playerPosition);
-        // print(playerLink);
-        // var responseString1 = docu
-        // // Converting the extracted titles into
-        // // string and returning a list of Strings
-        return [playerName, playerPosition, playerTeamImage];
-      } catch (e) {
-        return ['', '', 'ERROR!'];
-      }
-    } else {
-      return ['', '', 'ERROR: ${response.statusCode}.'];
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    var playerData = Provider.of<PlayerManager>(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Most Valuable Player')),
       body: Padding(
@@ -112,7 +69,13 @@ class _MyAppState extends State<MyApp> {
             isLoading
                 ? const CircularProgressIndicator()
                 : Column(
-                    children: [Image.network(result1)],
+                    children: [
+                      Image.network(result1),
+                      Image.network(result2),
+                      Text(
+                        result3,
+                      ),
+                    ],
                   ),
             SizedBox(height: MediaQuery.of(context).size.height * 0.08),
             MaterialButton(
@@ -121,10 +84,15 @@ class _MyAppState extends State<MyApp> {
                   isLoading = true;
                 });
 
-                final response = await extractData();
+                final response = await playerData.getRandomPlayer();
+                print(response[6]);
+                final secondPlayer = await playerData.getSecondPlayer(
+                    int.parse(response[6]), double.parse(response[5]));
 
                 setState(() {
                   result1 = response[2];
+                  result2 = response[3];
+                  result3 = response[5];
                   isLoading = false;
                 });
               },
---
+++ lib/providers/PlayerManager.dart
@@ -0,0 +1,173 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:http/http.dart' as http;
+
+class PlayerManager with ChangeNotifier {
+  Future<List<String>> getRandomPlayer() async {
+    // Getting the response from the targeted url
+    Random rnd = Random();
+    var playerRandNumb = rnd.nextInt(24);
+    var pageRandNumb = 1 + rnd.nextInt(19);
+
+    final response = await http.Client().get(Uri.parse(
+        'https://www.transfermarkt.co.uk/spieler-statistik/wertvollstespieler/marktwertetop?page=' +
+            pageRandNumb.toString()));
+
+    // Status Code 200 means response has been received successfully
+    if (response.statusCode == 200) {
+      // Getting the html document from the response
+      var document = parser.parse(response.body);
+      try {
+        var table = document
+            .getElementsByClassName('items')[0]
+            .children[1]; // thead or tbody (we want tbody so 1)
+        var player =
+            table.children[playerRandNumb]; //player index (row selector)
+        var playerDetails = player
+            .children[1]
+            // index number is 0 we want 1 for player details
+            .children[0] // td container
+            .children[0]; // inline table
+        var playerName = playerDetails
+            .children[0] // tbody child
+            .children[1] // first child is the image second is the name
+            .children[0] // inside the td there is a link for the player's name
+            .innerHtml;
+        var playerPosition = playerDetails.children[1].children[0].innerHtml;
+        var playerValue = player.children[5].children[0].innerHtml;
+        var playerValueNumb =
+            playerValue.replaceAll("£", "").replaceAll("m", "");
+        var playerLink = playerDetails
+            .children[0] // tbody child
+            .children[1] // first child is the image second is the name
+            .innerHtml
+            .split(
+                '"')[3]; // inside the td there is a link for the player's name
+        // print(playerValue);
+        // print(playerValueNumb);
+        var newLink = await http.Client()
+            .get(Uri.parse('https://www.transfermarkt.co.uk' + playerLink));
+
+        var newDocument = parser.parse(newLink.body);
+
+        var playerTeamImage = newDocument
+            .getElementsByClassName("dataZusatzImage")[0]
+            .children[0]
+            .innerHtml
+            .split('"')[1]
+            .split(',')[0]
+            .trim()
+            .split(' ')[0];
+        var playerImage = newDocument
+            .getElementsByClassName("modal-trigger")[0]
+            .innerHtml
+            .trim()
+            .split('"')[5];
+
+        return [
+          playerName,
+          playerPosition,
+          playerTeamImage,
+          playerImage,
+          playerValue,
+          playerValueNumb,
+          pageRandNumb.toString(),
+        ];
+      } catch (e) {
+        return ['', '', 'ERROR!'];
+      }
+    } else {
+      return ['', '', 'ERROR: ${response.statusCode}.'];
+    }
+  }
+
+  Future<List<String>> getSecondPlayer(
+      int otherPlayerPage, double otherPlayerValue) async {
+    // Getting the response from the targeted url
+    Random rnd = Random();
+    var playerRandNumb = rnd.nextInt(24);
+
+    int min = otherPlayerPage - 1;
+    int max = otherPlayerPage + 1;
+
+    var pageRandNumb = min + rnd.nextInt(max - min);
+
+    final response = await http.Client().get(Uri.parse(
+        'https://www.transfermarkt.co.uk/spieler-statistik/wertvollstespieler/marktwertetop?page=' +
+            pageRandNumb.toString()));
+
+    // Status Code 200 means response has been received successfully
+    if (response.statusCode == 200) {
+      // Getting the html document from the response
+      var document = parser.parse(response.body);
+      try {
+        var table = document
+            .getElementsByClassName('items')[0]
+            .children[1]; // thead or tbody (we want tbody so 1)
+        var player =
+            table.children[playerRandNumb]; //player index (row selector)
+        var playerDetails = player
+            .children[1]
+            // index number is 0 we want 1 for player details
+            .children[0] // td container
+            .children[0]; // inline table
+        var playerName = playerDetails
+            .children[0] // tbody child
+            .children[1] // first child is the image second is the name
+            .children[0] // inside the td there is a link for the player's name
+            .innerHtml;
+        var playerPosition = playerDetails.children[1].children[0].innerHtml;
+        var playerValue = player.children[5].children[0].innerHtml;
+        var playerValueNumb =
+            double.parse(playerValue.replaceAll("£", "").replaceAll("m", ""));
+        var playerLink = playerDetails
+            .children[0] // tbody child
+            .children[1] // first child is the image second is the name
+            .innerHtml
+            .split(
+                '"')[3]; // inside the td there is a link for the player's name
+        // print(playerValue);
+        // print(playerValueNumb);
+        var newLink = await http.Client()
+            .get(Uri.parse('https://www.transfermarkt.co.uk' + playerLink));
+
+        var newDocument = parser.parse(newLink.body);
+
+        var playerTeamImage = newDocument
+            .getElementsByClassName("dataZusatzImage")[0]
+            .children[0]
+            .innerHtml
+            .split('"')[1]
+            .split(',')[0]
+            .trim()
+            .split(' ')[0];
+        var playerImage = newDocument
+            .getElementsByClassName("modal-trigger")[0]
+            .innerHtml
+            .trim()
+            .split('"')[5];
+        print(playerName);
+        print(playerValue);
+        print(pageRandNumb);
+        if (otherPlayerValue == playerValueNumb) {
+          getSecondPlayer(otherPlayerPage, otherPlayerValue);
+        }
+        return [
+          playerName,
+          playerPosition,
+          playerTeamImage,
+          playerImage,
+          playerValue,
+          playerValueNumb.toString(),
+          pageRandNumb.toString()
+        ];
+      } catch (e) {
+        return ['', '', 'ERROR!'];
+      }
+    } else {
+      return ['', '', 'ERROR: ${response.statusCode}.'];
+    }
+  }
+}
